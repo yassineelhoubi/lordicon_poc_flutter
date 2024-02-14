4a7152b9cabfbac43ab5065382bfd304cbd9c78a
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2024-02-14 05:20:39 -0800

    integrate the lodicon

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,50 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MainApp());
+// }
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Hello World!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:lordicon/lordicon.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    var controller = IconController.assets('assets/icon-test.json');
+
+    controller.addStatusListener((status) {
+      if (status == ControllerStatus.ready) {
+        controller.playFromBeginning();
+      }
+    });
+
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: IconViewer(
+            controller: controller,
+            width: 200,
+            height: 200,
+          ),
         ),
       ),
     );
