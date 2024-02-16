bcb5d900efb492dc628d447ffc83a7c54adbdf30
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2024-02-16 10:35:54 +0100

    render container widget with bng background img

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -1,49 +1,76 @@
 import 'package:flutter/material.dart';
+import 'custom_button.dart';
 
 class TestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Page'),
-      ),
-      body: Center(
+      // appBar: AppBar(
+      //   title: Text('Second Page'),
+      // ),
+      body: Container(
+        padding: EdgeInsets.only(bottom: 10, top: 5, left: 5, right: 5),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // First Row - Image and Icon in the Middle
-                Container(
-                  width: 150,
-                  height: 150,
-                  color: Colors.blue, // Replace with your image or image URL
-                  child: Center(
-                    child: Icon(
-                      Icons.star,
-                      size: 50,
-                      color: Colors.white,
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                child: Center(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                        bottom: 50, top: 50, left: 10, right: 10),
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Center(child: MyCustomContainer()),
+                        Text(
+                          'Welcome to the Main Page!',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          'Welcome to the Main Page!',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ],
+              ),
             ),
-            SizedBox(height: 20),
-            // Second Row - Text
-            Text(
-              'This is the Second Page',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 20),
-            // Space between Button and Text
-            ElevatedButton(
+            CustomButton(
+              text: 'Retour accueil',
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: Text('Go Back to Main Page'),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyCustomContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200.0,
+      height: 200.0,
+      decoration: BoxDecoration(
+        // Set background image
+        image: DecorationImage(
+          image: AssetImage('assets/Panne.png'), // Replace with your image path
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Center(
+        // Icon in the middle of the container
+        child: Icon(
+          Icons.star,
+          size: 50.0,
+          color: Colors.white,
         ),
       ),
     );
