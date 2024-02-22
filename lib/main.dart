378b80879d771316a534e7cd50d8e79f627b5575
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2024-02-22 11:51:27 +0100

    adapt dark mode and customize colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:lordicon/lordicon.dart';
 import 'screens/main_page.dart';
 import 'package:provider/provider.dart';
 import 'dark_mode_provider.dart';
 
-void main() => runApp(
-      ChangeNotifierProvider(
-        create: (context) => DarkModeProvider(),
-        child: MyApp(),
-      ),
-    );
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
+    return ChangeNotifierProvider(
+      create: (context) => DarkModeProvider(),
+      child: Consumer<DarkModeProvider>(
+        builder: (context, darkModeProvider, child) {
+          ThemeData theme = darkModeProvider.isDarkMode
+              ? ThemeData.dark().copyWith(
+                  // Customize dark mode colors here
+                  scaffoldBackgroundColor: const Color(0xff00060a),
+                  colorScheme: const ColorScheme(
+                    brightness: Brightness.dark,
+                    primary: Color(0xff3dadff),
+                    onPrimary: Colors.white,
+                    secondary: Color(0xff788691),
+                    onSecondary: Colors.white,
+                    background: Color(0xff00060a),
+                    onBackground: Colors.white,
+                    surface: Color(0xff00060a),
+                    onSurface: Colors.white,
+                    error: Colors.red,
+                    onError: Colors.white,
+                  ),
+                )
+              : ThemeData.light().copyWith(
+                  scaffoldBackgroundColor: const Color(0xffffffff),
+                );
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: theme,
+            home: MainPage(),
+          );
+        },
+      ),
     );
   }
 }
--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -4,9 +4,6 @@ import 'package:lordicon_poc/widgets/custom_button.dart';
 import 'package:lordicon_poc/widgets/dark_mode_switch.dart';
 import 'package:provider/provider.dart';
 import '../dark_mode_provider.dart';
-// import 'package:provider/provider.dart';
-
-// import 'package:lordicon_poc/dark_mode_provider.dart';
 
 class MainPage extends StatelessWidget {
   @override
@@ -14,44 +11,41 @@ class MainPage extends StatelessWidget {
     final darkModeProvider = Provider.of<DarkModeProvider>(context);
     final theme =
         darkModeProvider.isDarkMode ? ThemeData.dark() : ThemeData.light();
-    return Theme(
-      data: theme,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Main Page"),
-          actions: [
-            Builder(
-              builder: (context) => DarkModeSwitch(),
-            ),
-          ],
-        ),
-        body: Container(
-          padding: EdgeInsets.only(bottom: 10, top: 5, left: 5, right: 5),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Container(
-                  // color: Colors.blue,
-                  child: Center(
-                    child: Text(
-                      'Welcome to the Main Page!',
-                      style: TextStyle(fontSize: 20),
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+        actions: [
+          Builder(
+            builder: (context) => DarkModeSwitch(),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(bottom: 10, top: 5, left: 5, right: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                // color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    'Welcome to the Main Page!',
+                    style: TextStyle(fontSize: 20),
                   ),
                 ),
               ),
-              CustomButton(
-                text: 'Go to Test',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TestPage()),
-                  );
-                },
-              ),
-            ],
-          ),
+            ),
+            CustomButton(
+              text: 'Go to Test',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TestPage()),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
